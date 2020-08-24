6a1f2f5caaccfc86d2dce0fa6614d6e57d46f1a8
QuisPic <56075863+QuisPic@users.noreply.github.com>
2020-08-24 20:57:31 +0300

    Deleted VHook.ts

--- VHook.ts
+++
@@ -1,9 +0,0 @@
-type AeNode = AVItem | CompItem | FolderItem | FootageItem | AVLayer | CameraLayer | LightLayer | ShapeLayer | TextLayer | Property<any> | PropertyGroup
-
-interface VHook<T extends AeNode> {
-  [key: string]: any
-  [key: number]: any
-
-  hook?(node: T, propertyName: string, previous?: VHook<T>): void;
-  unhook?(node: T, propertyName: string, previous?: VHook<T>): void;
-}
